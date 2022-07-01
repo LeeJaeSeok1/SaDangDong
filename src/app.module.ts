b8693573a2e1e041fb2c2f5bd843561c82b8dc84
LeeJaeSeok1 <98381653+LeeJaeSeok1@users.noreply.github.com>
2022-07-02 03:59:13 +0900

    커밋

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { ConfigModule } from '@nestjs/config';
             }),UsersModule, CollectionsModule, ItemsModule, AuctionsModule, SearchModule,
               ExploreModule, SellModule, FavoritesModule, EventsModule, TypeOrmModule.forRoot({
               type: 'mysql',
-              host: 'localhost',
+              host: '13.125.82.28',
               username: process.env.DB_NAME,
               password: process.env.DB_PASSWORD,
               database: 'user',
