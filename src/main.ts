78bef79aee9861ca94c9722c816c60e224a6ecca
lojy4231 <lojy4231@gmail.com>
2022-07-14 22:39:05 +0900

    Feat: swagger에서 jwt 토큰값 입력 기능 추가

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,15 @@ async function bootstrap() {
         .setTitle("SaDangDong API")
         .setDescription("사당동(NFT Auction 프로젝트)를 위한 API 문서입니다.")
         .setVersion("1.0")
-        .addCookieAuth("connect.sid")
+        .addBearerAuth(
+            {
+                type: "http",
+                scheme: "bearer",
+                name: "JWT",
+                in: "header",
+            },
+            "access-token",
+        )
         .build();
     const document = SwaggerModule.createDocument(app, swagger);
     SwaggerModule.setup("api", app, document);
