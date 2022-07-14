33b58e0d4d502dc5fae0e7a7da1e061f2c5b5b52
lojy4231 <lojy4231@gmail.com>
2022-07-14 22:41:59 +0900

    Feat: 이미지 DB에 저장시 타켓 추가

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, UploadedFiles, UseInterceptors } from "@nestjs/common";
-import { FileInterceptor, FilesInterceptor } from "@nestjs/platform-express";
+import { Body, Controller, Post, UploadedFiles, UseInterceptors } from "@nestjs/common";
+import { FilesInterceptor } from "@nestjs/platform-express";
 import { ApiBody, ApiConsumes, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { ImagesService } from "./images.service";
 import { storage } from "src/config/multerS3.config";
+import { CreateImageDto } from "./dto/createImage.dto";
 
 @ApiTags("Image")
 @Controller("api/images")
@@ -15,18 +16,20 @@ export class ImagesController {
         schema: {
             type: "object",
             properties: {
-                file: {
-                    type: "string",
-                    format: "binary",
+                files: {
+                    type: "array",
+                    items: {
+                        type: "string",
+                        format: "binary",
+                    },
                 },
             },
         },
     })
-    @UseInterceptors(FileInterceptor("file"))
     @Post()
     @UseInterceptors(FilesInterceptor("files", 3, { storage: storage }))
-    async uploadImage(@UploadedFiles() files: Express.Multer.File[]) {
-        return await this.imagesService.uploadImage(files);
+    async uploadImage(@UploadedFiles() files: Express.Multer.File[], @Body() createImageDto: CreateImageDto) {
+        return await this.imagesService.uploadImage(files, createImageDto);
     }
 
     //     @ApiOperation({ summary: "이미지 업로드", description: "이미지 업로드 페이지" })
