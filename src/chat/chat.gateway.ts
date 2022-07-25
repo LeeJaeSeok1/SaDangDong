e1f34c3bfac3b518ae801eaa39ad34fd22742b33
LeeJaeSeok1 <98381653+LeeJaeSeok1@users.noreply.github.com>
2022-07-25 20:15:42 +0900

    채팅 샘플

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,43 +1,86 @@
-import { Logger } from '@nestjs/common';
-import {
-  OnGatewayConnection,
-  OnGatewayDisconnect,
-  OnGatewayInit,
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
-} from '@nestjs/websockets';
+import { Logger, CACHE_MANAGER, CacheTTL,  UseInterceptors, HttpException, Inject, Injectable } from '@nestjs/common';
+import { OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { Cache } from 'cache-manager';
+// import { HttpCacheInterceptor } from '../interceptor/http-cache.interceptor';
 
-@WebSocketGateway({ namespace: 'chat' })
-export class ChatGateway
-  implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit {
-  private static readonly logger = new Logger(ChatGateway.name);
+@Injectable()
+@WebSocketGateway({ namespace: '/chat' })
+export class ChatGateway implements OnGatewayInit {
+  constructor(
+    @Inject(CACHE_MANAGER) private redisManager: Cache
+  ) {}
 
-  @WebSocketServer()
-  server: Server;
+  @WebSocketServer() wss: Server;
 
-  afterInit() {
-    ChatGateway.logger.debug(`Socket Server Init Complete`);
+  private logger: Logger = new Logger('ChatGateway')
+
+  afterInit(server: any) {
+    this.logger.log('Initialized!');
   }
 
-  handleConnection(client: Socket) {
-    ChatGateway.logger.debug(
-      `${client.id}(${client.handshake.query['username']}) is connected!`,
-    );
-
-    this.server.emit('msgToClient', {
-      name: `admin`,
-      text: `join chat.`,
-    });
+  // @CacheTTL(600)
+  // @UseInterceptors(HttpCacheInterceptor)
+  @SubscribeMessage('chatToServer')
+  async handleMessage(client: Socket, message: { sender: string, room: string, message: string}) {
+    this.wss.to(message.room).emit('chatToClient', message);
+    await this.redisManager.set('api: chat', message, { ttl: 60 });
   }
 
-  handleDisconnect(client: Socket) {
-    ChatGateway.logger.debug(`${client.id} is disconnected...`);
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(client: Socket, room: string) {
+    client.join(room);
+    client.emit('joinedRoom', room);
   }
-
-  @SubscribeMessage('msgToServer')
-  handleMessage(client: Socket, message: { name: string; text: string }): void {
-    this.server.emit('msgToClient', message);
+  
+  @SubscribeMessage('leaveRoom')
+  handleLeaveRoom(client: Socket, room: string) {
+    client.leave(room);
+    client.emit('leftRoom', room);
   }
 }
+
+
+// import { Logger } from '@nestjs/common';
+// import {
+//   OnGatewayConnection,
+//   OnGatewayDisconnect,
+//   OnGatewayInit,
+//   SubscribeMessage,
+//   WebSocketGateway,
+//   WebSocketServer,
+// } from '@nestjs/websockets';
+// import { Server, Socket } from 'socket.io';
+
+// @WebSocketGateway({ namespace: 'chat' })
+// export class ChatGateway
+//   implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit {
+//   private static readonly logger = new Logger(ChatGateway.name);
+
+//   @WebSocketServer()
+//   server: Server;
+
+//   afterInit() {
+//     ChatGateway.logger.debug(`Socket Server Init Complete`);
+//   }
+
+//   handleConnection(client: Socket) {
+//     ChatGateway.logger.debug(
+//       `${client.id}(${client.handshake.query['username']}) is connected!`,
+//     );
+
+//     this.server.emit('msgToClient', {
+//       name: `admin`,
+//       text: `join chat.`,
+//     });
+//   }
+
+//   handleDisconnect(client: Socket) {
+//     ChatGateway.logger.debug(`${client.id} is disconnected...`);
+//   }
+
+//   @SubscribeMessage('msgToServer')
+//   handleMessage(client: Socket, message: { name: string; text: string }): void {
+//     this.server.emit('msgToClient', message);
+//   }
+// }
--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,11 +1,22 @@
 
-
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
+import * as redisStore from 'cache-manager-ioredis'
+
 
 @Module({
-  imports: [],
+  imports: [
+		CacheModule.registerAsync({
+		useFactory: () => ({
+	      store: redisStore,
+    	  host: 'localhost',
+	      port: 6379,		// Redis의 기본 포트번호이다.
+   		 })
+		})
+      ],
+  controllers: [],
   providers: [ChatGateway],
-  exports: [],
 })
 export class ChatModule {}
+
+
