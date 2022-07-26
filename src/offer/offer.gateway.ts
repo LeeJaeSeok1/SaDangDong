b17bd47ded73e321a38845fbc4cd21c12b1a27de
eunchong2lee <tlfqjrjs1234@naver.com>
2022-07-26 18:46:38 +0800

    [develop] Offer_feature

--- src/offer/offer.gateway.ts
+++ src/offer/offer.gateway.ts
@@ -14,32 +14,61 @@ import { onlineMap } from "./onlineMap";
 import { OfferService } from "./offer.service";
 import { AuthToken } from "src/config/auth.decorator";
 
+// @WebSocketGateway({ namespace: "offer", cors: { origin: "*" } })
+// export class OfferGateway {
+//     @WebSocketServer() public server: Server;
+
+//     constructor(private readonly offerService: OfferService) {}
+
+//     @SubscribeMessage("OffertoServer")
+//     async createOffer(
+//         @MessageBody() data: { price: number; mycoin: number },
+//         @AuthToken() address: string,
+//         @Param("auction_id") auction_id: string,
+//     ) {
+//         console.log(data, address, auction_id);
+//         const myoffer = await this.offerService.createOffer(address, data, auction_id);
+//         this.server.emit("offer", myoffer);
+//         return myoffer;
+//     }
+
+//     @SubscribeMessage("findAllOffers")
+//     findAllOffer(@AuthToken() address: string, @Param("auction_id") auction_id: string) {
+//         return this.offerService.findAllOffer(address, auction_id);
+//     }
+
+//     @SubscribeMessage("join")
+//     joinRoom(@MessageBody() data: { auction_id: number; address: string }, @ConnectedSocket() client: Socket) {
+//         console.log(data, client.id);
+//         return this.offerService.joinRoom(data, client.id);
+//     }
+// }
+
 @WebSocketGateway({ namespace: "offer", cors: { origin: "*" } })
-export class OfferGateway {
-    @WebSocketServer() public server: Server;
+export class OfferGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+    constructor(private offerService: OfferService) {}
 
-    constructor(private readonly offerService: OfferService) {}
+    @WebSocketServer() server: Server;
 
-    @SubscribeMessage("OffertoServer")
-    async createOffer(
-        @MessageBody() data: { price: number; mycoin: number },
-        @AuthToken() address: string,
-        @Param("auction_id") auction_id: string,
-    ) {
-        console.log(data, address, auction_id);
-        const myoffer = await this.offerService.createOffer(address, data, auction_id);
-        this.server.emit("offer", myoffer);
-        return myoffer;
+    @SubscribeMessage("sendOffer")
+    async handleSendMessage(client: Socket, { address, data, auction_id }): Promise<void> {
+        await this.offerService.createOffer(address, data, auction_id);
+        // data : {price, mycoin}
+        this.server.emit("recMessage", data);
     }
 
-    @SubscribeMessage("findAllOffers")
-    findAllOffer(@AuthToken() address: string, @Param("auction_id") auction_id: string) {
-        return this.offerService.findAllOffer(address, auction_id);
+    afterInit(server: Server) {
+        console.log(server);
+        //Do stuffs
     }
 
-    @SubscribeMessage("join")
-    joinRoom(@MessageBody() data: { auction_id: number; address: string }, @ConnectedSocket() client: Socket) {
-        console.log(data, client.id);
-        return this.offerService.joinRoom(data, client.id);
+    handleDisconnect(client: Socket) {
+        console.log(`Disconnected: ${client.id}`);
+        //Do stuffs
+    }
+
+    handleConnection(client: Socket, ...args: any[]) {
+        console.log(`Connected ${client.id}`);
+        //Do stuffs
     }
 }
