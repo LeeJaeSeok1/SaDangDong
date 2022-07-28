f24f1a6d9370936de36bbbfb471787119a651534
eunchong2lee <tlfqjrjs1234@naver.com>
2022-07-28 16:57:01 +0900

    [REDIS] delete

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,19 +1,19 @@
-import { CacheModule, Module } from "@nestjs/common";
-// import { ChatGateway } from "./chat.gateway";
-import * as redisStore from "cache-manager-ioredis";
-import { ChatGateway } from "./chat.controller";
+// import { CacheModule, Module } from "@nestjs/common";
+// // import { ChatGateway } from "./chat.gateway";
+// import * as redisStore from "cache-manager-ioredis";
+// import { ChatGateway } from "./chat.controller";
 
-@Module({
-    imports: [
-        CacheModule.registerAsync({
-            useFactory: () => ({
-                store: redisStore,
-                host: "localhost",
-                port: 6379, // Redis의 기본 포트번호이다.
-            }),
-        }),
-    ],
-    controllers: [ChatGateway],
-    providers: [],
-})
-export class ChatModule {}
+// @Module({
+//     imports: [
+//         CacheModule.registerAsync({
+//             useFactory: () => ({
+//                 store: redisStore,
+//                 host: "localhost",
+//                 port: 6379, // Redis의 기본 포트번호이다.
+//             }),
+//         }),
+//     ],
+//     controllers: [ChatGateway],
+//     providers: [],
+// })
+// export class ChatModule {}
