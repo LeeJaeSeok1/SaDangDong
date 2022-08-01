124c76c2a8154b503c016d3d7e8f48feeea032de
eunchong2lee <tlfqjrjs1234@naver.com>
2022-08-01 11:26:00 +0900

    [DEV] jsonrpc

--- src/jsonrpc/jsonrpc.service.ts
+++ src/jsonrpc/jsonrpc.service.ts
@@ -4,6 +4,8 @@ import { Ethereum } from "./entities/ethereum.entity";
 import { Repository } from "typeorm";
 import { HttpService } from "@nestjs/axios";
 import * as dotenv from "dotenv";
+import { lastValueFrom } from "rxjs/internal/lastValueFrom";
+import { map } from "rxjs/internal/operators/map";
 
 @Injectable()
 export class JsonRpcService {
@@ -17,15 +19,45 @@ export class JsonRpcService {
 
     async getethereumcoin(address: string) {
         try {
+            if (address == `"NOT DEFINED"`) {
+                return Object.assign({
+                    statusCode: 400,
+                    success: true,
+                    statusMsg: `메타마스크에 연결하십시오.`,
+                });
+            }
+
+            const [check_getethereum] = await this.ethereumRepository.query(`
+            SELECT *
+            FROM ethereum
+            WHERE address = "${address}"
+            `);
+
+            if (check_getethereum) {
+                const new_date = new Date();
+                const ended_at = new Date(check_getethereum.get_at);
+                ended_at.setDate(ended_at.getDate() + 1);
+                if (new_date < ended_at) {
+                    const remained_time = ended_at.getTime() - new_date.getTime();
+                    const fixed_time = new Date(remained_time).toISOString().split("T")[1].split(".")[0];
+                    return Object.assign({
+                        statusCode: 401,
+                        success: true,
+                        statusMsg: `이더리움이 지급시간까지 ${fixed_time} 남았습니다.`,
+                    });
+                }
+            }
+
             const url = process.env.BLOCKCHAIN_SERVER;
+            const from_address = process.env.BLOCKCHAIN_ADDRESS;
             const data = {
                 jsonrpc: "2.0",
                 method: "eth_sendTransaction",
                 params: [
                     {
-                        from: "eth_accounts[0]",
+                        from: `${from_address}`,
                         to: `${address}`,
-                        value: "0x821ab0d441498000",
+                        value: "0x1221ab0d441498000",
                         // data: 컴파일 주소,
                     },
                 ],
@@ -36,18 +68,64 @@ export class JsonRpcService {
                     "Content-Type": "application/json",
                 },
             };
-            const hello = await this.httpService.post(url, data, option);
-            console.log(hello);
-            console.log(address);
-            console.log(process.env.BLOCKCHAIN_SERVER);
-            return hello;
+            const result = await lastValueFrom(
+                this.httpService.post(url, data, option).pipe(
+                    map((response) => {
+                        return response.data;
+                    }),
+                ),
+            );
+            if (result.error) {
+                return Object.assign({
+                    statusCode: 400,
+                    success: true,
+                    statusMsg: `주소 또는 요청이 잘못되었습니다.`,
+                });
+            }
+
+            const loweraddress = address.toLowerCase();
+            if (!check_getethereum) {
+                const getethereumtime = new Ethereum();
+
+                getethereumtime.address = loweraddress;
+                getethereumtime.get_at = new Date();
+
+                await this.ethereumRepository.save(getethereumtime);
+            } else {
+                check_getethereum.get_at = new Date();
+                await this.ethereumRepository.update(check_getethereum.id, check_getethereum);
+            }
+
+            return Object.assign({
+                statusCode: 200,
+                success: true,
+                statusMsg: `몇 초 뒤에 이더리움이 지급됩니다.`,
+            });
         } catch (error) {
             throw new BadRequestException(error.message);
         }
     }
 
     async hellofunction() {
-        const response = await this.httpService.get(`https://m2.melon.com/cds/main/mobile4web/main_chart.htm`);
+        const url = process.env.BLOCKCHAIN_SERVER;
+        const data = {
+            jsonrpc: "2.0",
+            method: "eth_sendTransaction",
+            params: [
+                {
+                    jsonrpc: "2.0",
+                    method: "miner_stop",
+                    params: [],
+                },
+            ],
+            id: 1,
+        };
+        const option = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+        const response = await this.httpService.get(url, data);
         console.log(response);
         return response;
     }
