65a21950692f2fb6ea54d689ecb62faa33a71871
eunchong2lee <tlfqjrjs1234@naver.com>
2022-08-03 02:26:57 +0900

    [DEV] main

--- src/main.ts
+++ src/main.ts
@@ -30,8 +30,9 @@ async function bootstrap() {
     );
 
     app.enableCors({
-        origin: "*",
+        origin: ["https://sadangdong.com/", "localhost:3030"],
         methods: "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS",
+        allowedHeaders: ["access-control-allow-origin", "X-Requested_With", "Content-Type", "Accept"],
         credentials: true,
     });
     await app.listen(port);
