b453382f587cbc120d6d3325418d8649077b5068
eunchong2lee <tlfqjrjs1234@naver.com>
2022-08-03 10:18:34 +0900

    [FIX] CORS

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { Credentials } from "aws-sdk";
 // import { RedisIoAdapter } from './chat/redis.adapter';
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
     const configService = app.get(ConfigService);
     const port = configService.get<string>("server.port");
