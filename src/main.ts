0908b201dc340ca5d5429f496c42cde461657093
eunchong2lee <tlfqjrjs1234@naver.com>
2022-08-03 10:22:32 +0900

    [FIX] CORS

--- src/main.ts
+++ src/main.ts
@@ -11,19 +11,6 @@ import { Credentials } from "aws-sdk";
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-    const configService = app.get(ConfigService);
-    const port = configService.get<string>("server.port");
-    // app.useWebSocketAdapter(new RedisIoAdapter(app));
-    // app.useStaticAssets(join(__dirname, '..', 'static'));
-
-    const swagger = new DocumentBuilder()
-        .setTitle("SaDangDong API")
-        .setDescription("사당동(NFT Auction 프로젝트)를 위한 API 문서입니다.")
-        .setVersion("1.0")
-        .build();
-    const document = SwaggerModule.createDocument(app, swagger);
-    SwaggerModule.setup("api", app, document);
-
     app.useGlobalPipes(
         new ValidationPipe({
             transform: true,
@@ -36,6 +23,19 @@ async function bootstrap() {
         methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
         credentials: true,
     });
+
+    const configService = app.get(ConfigService);
+    const port = configService.get<string>("server.port");
+    // app.useWebSocketAdapter(new RedisIoAdapter(app));
+    // app.useStaticAssets(join(__dirname, '..', 'static'));
+
+    const swagger = new DocumentBuilder()
+        .setTitle("SaDangDong API")
+        .setDescription("사당동(NFT Auction 프로젝트)를 위한 API 문서입니다.")
+        .setVersion("1.0")
+        .build();
+    const document = SwaggerModule.createDocument(app, swagger);
+    SwaggerModule.setup("api", app, document);
 
     await app.listen(port);
     Logger.log(`Application runnin on port ${port}`);
